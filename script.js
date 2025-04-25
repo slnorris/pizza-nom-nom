58bb95d3069af96ffb2b56f36f376fdf269a987d
Shelby Norris <shelbs9109@gmail.com>
2025-04-24 21:48:51 -0500

    Created functions for event listener, edited html and css, submit event successfully loads input into table row

--- script.js
+++ script.js
@@ -10,18 +10,36 @@ function pizzaOrderLog(event) {
   let customerLastName = document.getElementById("lname").value;
   let pizzaSize = document.getElementById("sizes").value;
   let crustType = document.getElementById("crust").value;
-  let toppingChoices = document.getElementById("toppings").value;
+  let toppingChoices = document.getElementById("toppings").querySelectorAll('input[type="checkbox"]');
+  console.log(toppingChoices);
   let deliveryInstructions = document.getElementById("instructions").value;
 
- let newOrder = pizzaOrderTable.insertRow();
- let nameData = newOrder.insertCell(0);
- nameData.textContent = `${customerFirstName} ${customerLastName}`;
- let sizeData = newOrder.insertCell(1);
- sizeData.textContent = pizzaSize;
- let crustData = newOrder.insertCell(2);
- crustData.innerHTML = crustType;
- let toppingData = newOrder.insertCell(3);
- toppingData.innerHTML = toppingChoices;
- let instructionData = newOrder.insertCell(4);
- instructionData.innerHTML = deliveryInstructions;
+  let newOrder = pizzaOrderTable.insertRow();
+
+  let nameData = newOrder.insertCell(0);
+  nameData.textContent = `${customerFirstName} ${customerLastName}`;
+
+  let sizeData = newOrder.insertCell(1);
+  sizeData.textContent = pizzaSize;
+
+  let crustData = newOrder.insertCell(2);
+  crustData.textContent = crustType;
+
+  let toppingData = newOrder.insertCell(3);
+  let toppingString = ""
+  for (let toppingElement of toppingChoices) {
+      if (toppingElement.checked) {
+        toppingString += toppingElement.value + ", ";
+        console.log(toppingElement)
+      }
+  }
+
+  toppingData.textContent = toppingString.replace(/, $/, "");
+
+  let instructionData = newOrder.insertCell(4);
+  instructionData.textContent = deliveryInstructions;
+  if (deliveryInstructions === "") {
+    instructionData.textContent = "none";
+  }
 }
+
